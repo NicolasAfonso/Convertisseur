c512f4c931e645bd0f3ea720817c89aa3d271b89
Nicolas Afonso <afonso.nicolas@live.fr>
2018-10-31 18:46:04 +0100

    UIViewController added for second screen

---
+++ Convertisseur/ConvertControllerViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ConvertControllerViewController.swift
+//  Convertisseur
+//
+//  Created by Afonso, Nicolas on 31/10/2018.
+//  Copyright © 2018 CA Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class ConvertControllerViewController: UIViewController {
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var inputView: UIView!
+    
+    @IBOutlet weak var todoLabel: UILabel!
+    
+    @IBOutlet weak var dataTextField: UITextField!
+    @IBOutlet weak var outputView: UIView!
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func switchAction(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+    }
+    @IBAction func updatedTextField(_ sender: UITextField) {
+    }
+    
+}
