6681d6423cdffc8194b0a6ba38a662339f564008
Nicolas Afonso <afonso.nicolas@live.fr>
2018-11-05 14:39:06 +0100

    Gesture support for keyboard auto hide

--- Convertisseur/ConvertControllerViewController.swift
+++ Convertisseur/ConvertControllerViewController.swift
@@ -10,31 +10,78 @@ import UIKit
 
 class ConvertControllerViewController: UIViewController {
 
-    @IBOutlet weak var title: UILabel!
-    
-    @IBOutlet weak var inputView: UIView!
-    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var inputTextField: UIView!
     @IBOutlet weak var todoLabel: UILabel!
-    
     @IBOutlet weak var dataTextField: UITextField!
     @IBOutlet weak var outputView: UIView!
-    
-    
     @IBOutlet weak var outputLabel: UILabel!
     
+    var type:String?
+    var views :[UIView] = []
+    var isSwitched : Bool = false
     
+    // Wording
+    let eurosW : String = "euros"
+    let dollarsW : String = "dollars"
+    let celciusW : String = "celcius"
+    let fahrenheitW : String = "fahrenheits"
+    let kilometersW : String = "kilomètres"
+    let milesW : String = "miles"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let convertType = type {
+        titleLabel.text = "Conversion de \(convertType)"
+        
+        views = [inputTextField, outputView]
+        setCornerRadiusOfViews(radius: 10, viewsArray: views)
+        
+        switch type{
+        case DEVISE : setTitleLabel(eurosW, dollarsW) ; todoLabel.text = "Entrez la somme à convertir"
+        case TEMPERATURE : setTitleLabel(celciusW, fahrenheitW) ; todoLabel.text = "Entrez la température à convertir"
+        case DISTANCE : setTitleLabel(kilometersW, milesW) ;  todoLabel.text = "Entrez la distance à convertir"
+        default: break
+            }
+            
+            
+            let tap =  UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            view.addGestureRecognizer(tap)
+        }
+        else { dismiss(animated: true, completion: nil) }
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func setCornerRadiusOfViews (radius : Int, viewsArray : [UIView]){
+        for v in viewsArray {
+            v.layer.cornerRadius = CGFloat(radius)        }
+    }
+    
+    func setTitleLabel ( _ sourceToConvert : String, _ destinationToConvert : String ){
+        if !(isSwitched) {
+        titleLabel.text = "Conversion de \(sourceToConvert) vers \(destinationToConvert)"
+        }
+        else {
+              titleLabel.text = "Conversion de \(destinationToConvert) vers \(sourceToConvert)"
+        }
     }
     
 
     @IBAction func switchAction(_ sender: Any) {
+        isSwitched = !isSwitched
+        switch type{
+        case DEVISE : setTitleLabel(eurosW, dollarsW)
+        case TEMPERATURE : setTitleLabel(celciusW, fahrenheitW)
+        case DISTANCE : setTitleLabel(kilometersW, milesW)
+        default: break
+        }
     }
     
     @IBAction func backButtonAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     @IBAction func updatedTextField(_ sender: UITextField) {
     }
--- Convertisseur/ViewController.swift
+++ Convertisseur/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+let DEVISE = "Devise"
+let TEMPERATURE = "Température"
+let DISTANCE = "Distance"
+
 class ViewController: UIViewController {
     
     
@@ -15,13 +19,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var distanceView: UIView!
     @IBOutlet weak var temperatureView: UIView!
     
+    let segueID = "Convert"
+    var views : [UIView] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        views = [deviseView, distanceView, temperatureView]
+        setCornerRadiusOfViews(radius: 10, viewsArray: views)
+            }
+    
+    
+    func setCornerRadiusOfViews (radius : Int, viewsArray : [UIView]){
+        for v in viewsArray {
+            v.layer.cornerRadius = CGFloat(radius)        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueID {
+            if let convertController = segue.destination as? ConvertControllerViewController{
+               convertController.type = sender as? String
+            }
+        }
     }
 
 
-    @IBAction func buttonAction(_ sender: Any) {
+    @IBAction func buttonAction(_ sender: UIButton) {
+        switch sender.tag {
+            case 0: performSegue(withIdentifier: segueID, sender: DEVISE)
+            case 1: performSegue(withIdentifier: segueID, sender: DISTANCE)
+            case 2: performSegue(withIdentifier: segueID, sender: TEMPERATURE)
+        default: break
+        }
     }
 }
 
